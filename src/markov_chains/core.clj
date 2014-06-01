fdc1c9ad060a85be2cbf8057f7975b08233b7d61
Stephen Caudill <scaudill@gmail.com>
2014-06-01 15:00:10 -0600

    Recurse to make a probabilistic nonsense sentence

--- src/markov_chains/core.clj
+++ src/markov_chains/core.clj
@@ -18,5 +18,22 @@
   (into {} (map (fn [[k v]] [k (frequencies (map second v))])
                 (successive-words word-seq))))
 
-(defn -main [word]
-   (println (get (process (words metamorphosis-text)) word)))
+(defn first-words [word-map]
+  (filter #(re-matches #"^[A-Z].*$" %) (keys word-map)))
+
+(defn seed [word-map]
+  (rand-nth (first-words word-map)))
+
+(defn next-word [word word-map]
+  (rand-nth (flatten (map (fn [[k v]] (repeat v k)) (get word-map word)))))
+
+(defn recur-words [word word-map iterations]
+  (cons word (if (< iterations 12)
+               (let [successive (next-word word word-map)
+                     next-iter (inc iterations)]
+                 (recur-words successive word-map next-iter)))))
+
+(defn -main []
+  (let [word-map (process (words metamorphosis-text))
+        stem (seed word-map)]
+    (apply println (recur-words stem word-map 0))))
